1cc2dde2297bfd9b2f8f7face2bdef58f0e86407
Aarux <gustav_bergman95@hotmail.com>
2019-09-05 10:42:27 +0200

    init repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import LikeButton from "./LikeButton";
+
+function App() {
+  return (
+    <div className="App">
+      <LikeButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LikeButton.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-family: "Verdana", sans-serif;
+  font-weight: normal;
+  font-size: 1.5rem;
+  color: #000;
+  width: 20%;
+  margin: 5px;
+  padding: 5px 25px;
+  background-color: #fff;
+  transition: 0.3s;
+  border: #000 1px solid;
+
+  ${({ liked }) =>
+    liked &&
+    `
+    background-color: #1f2f3c;
+    color: #f1f1f1;
+    font-weight:bold;
+  `}
+`;
+
+export default class Toggle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 100,
+      liked: false
+    };
+    this.updateCount = this.updateCount.bind(this);
+  }
+
+  updateCount() {
+    if (!this.state.liked) {
+      this.setState((prevState, props) => {
+        return {
+          count: prevState.count + 1,
+          liked: true
+        };
+      });
+    } else {
+      this.setState((prevState, props) => {
+        return {
+          count: prevState.count - 1,
+          liked: false
+        };
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Button liked={this.state.liked} onClick={this.updateCount}>
+          Like | {this.state.count}
+        </Button>
+      </div>
+    );
+  }
+}
